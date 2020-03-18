4aa2b6c289d48eaaadda1b04041f0c6b9ceef67a
WilliamNB <59480886+WilliamNB@users.noreply.github.com>
2020-03-18 22:54:17 +0000

    spawn circles initial

---
+++ IOS circles/Assets/Scripts/CircleController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleController : MonoBehaviour
+{
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+
+                if (hit.transform.position.x == this.transform.position.x)
+                {
+                   // this.setState(1);
+                    this.name = "clicked";
+                }
+
+                /**              if (hit.transform.name == "circle2(Clone)" && circNum == 2)
+                              {
+                                  this.setState(1);
+                                  this.name = "clicked";
+                              }
+
+                              Debug.Log(hit.collider.gameObject.name);
+                              if (hit.transform.name == "circle3(Clone)" && circNum == 3)
+                              {
+                                  this.setState(1);
+                                  this.name = "clicked";
+                              }
+
+                              Debug.Log(hit.collider.gameObject.name);
+                              if (hit.transform.name == "circle4(Clone)" && circNum == 4)
+                              {
+                                  this.setState(1);
+                                  this.name = "";
+                              }
+                         */
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ IOS circles/Assets/Scripts/SpawnCircles.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCircles : MonoBehaviour
+{
+    public GameObject circlePrefab1;
+    public GameObject circlePrefab2;
+    public GameObject circlePrefab3;
+    public GameObject circlePrefab4;
+    public int spawnRate = 3;
+    private Vector2 screenBounds;
+    private float fix = 1.2F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(circleWave());
+    }
+
+
+    private void spawn()
+    {
+        int range = Random.Range(1, 5);
+        switch (1)
+        {
+            case 1:
+                GameObject test = Instantiate(circlePrefab1) as GameObject;
+                test.transform.position = new Vector2(Random.Range(-fix, fix), screenBounds.y);
+                break;
+            case 2:
+                GameObject test2 = Instantiate(circlePrefab2) as GameObject;
+                test2.transform.position = new Vector2(Random.Range(-fix, fix), screenBounds.y);
+                break;
+            case 3:
+                GameObject test3 = Instantiate(circlePrefab3) as GameObject;
+                test3.transform.position = new Vector2(Random.Range(-fix, fix), screenBounds.y);
+                break;
+            case 4:
+                GameObject test4 = Instantiate(circlePrefab4) as GameObject;
+                test4.transform.position = new Vector2(Random.Range(-fix, fix), screenBounds.y);
+                break;
+        }
+
+    }
+
+    IEnumerator circleWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            spawn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
