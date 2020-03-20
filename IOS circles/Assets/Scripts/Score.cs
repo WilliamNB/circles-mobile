c258687f4c8af9f174bbeb4464622766f20bcc17
WilliamNB <59480886+WilliamNB@users.noreply.github.com>
2020-03-20 14:29:53 +0000

    Add score script
    Just script not UI element

---
+++ IOS circles/Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    static int scoreValue;
+    public Text currentScore;
+    int objectCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = GetComponent<Text>();
+        scoreValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentScore.text = scoreValue.ToString();
+    }
+
+    public static void IncreaseScore()
+    {
+        scoreValue += 1;
+    }
+}
